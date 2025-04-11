4fa802b2839524487f922324e493419fc3d14ac8
Ivan <igutierrezcontreras@danielcastelao.org>
2025-04-11 10:49:50 +0200

    segundo_commit_MVC

---
+++ src/TEST.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+class TEST {
+
+    @Test
+    void cambiarVelocidadShouldThrowExceptionIfCocheDoesNotExist() {
+    }
+
+
+    @Test
+    void getCoche() {
+    }
+
+
+
+    @Test
+    void getVelocidadShouldReturnCorrectVelocidad() {
+    }
+
+    @Test
+    void crearCoche() {
+    }
+
+    @Test
+    void cambiarVelocidadShouldUpdateCocheVelocidad() {
+    }
+}
+
